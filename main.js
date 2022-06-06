7b000ba1c072155860424cf37fe496a52055b4ca
Parker Duncan <100903172+ParkerDuncanDev@users.noreply.github.com>
2022-06-05 19:59:27 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,73 @@
+//Query selector variables and eventListeners
+let slashButton = document.querySelector('#slash')
+let saveButton = document.querySelector('#save')
+let settingsButton = document.querySelector('#settings')
+let nextButton = document.querySelector('#next')
+let poster = document.querySelector('#poster')
+let description = document.querySelector('#description')
+let title = document.querySelector('#title')
+let databaseRating = document.querySelector('#dbrating')
+let slashrRating = document.querySelector('#slrating')
+
+
+
+slashButton.addEventListener('click', slashMovie)
+saveButton.addEventListener('click', saveMovie)
+settingsButton.addEventListener('click', openSettings)
+nextButton.addEventListener('click', getNewMovie)
+
+//functions TEMPORARY
+function slashMovie(){
+  console.log('slash button works')
+}
+function saveMovie(){
+  console.log('save button works')
+}
+function openSettings(){
+  console.log('settings button works')
+}
+
+//serverRequests
+async function getNewMovie(){
+  //generate random number
+  let randPage = Math.ceil(Math.random() * 500)
+  console.log(randPage)
+  //fetch a random page of movies from TMDB API
+ const res = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=585021f63795aea89ca90be073375167&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${randPage}&with_genres=27&with_watch_monetization_types=flatrate`)
+ const data = await res.json()
+
+ console.log(data);
+  //movie selection and property variables
+  let selectedMovie = data.results[10]
+  let newTitle = selectedMovie.title
+  let newPosterPath = selectedMovie.poster_path
+  let newDescription = selectedMovie.overview
+  let newDatabaseRating = selectedMovie.vote_average
+  //TEMPORARY, WILL BE CHANGED WITH SERVERSIDE INTEGRATION
+  let newSlashrRating = selectedMovie.vote_average
+  //DOM changes
+  poster.src = `https://image.tmdb.org/t/p/w500/${newPosterPath}`
+  description.innerText = newDescription
+  title.innerText = newTitle
+  databaseRating.innerText = newDatabaseRating
+  
+  
+  
+
+
+}
+  // let randPage = Math.ceil(Math.random() * 500)
+  // console.log(randPage)
+  // const res = await fetch(`/api?pagenumber=${randPage}`)
+  // const data = await res.json()
+
+  // console.log(data);
+// async function getNewMovie(){
+//   let randPage = Math.ceil(Math.random() * 500)
+//   console.log(randPage)
+//   const res = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=585021f63795aea89ca90be073375167&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${randPage}&with_genres=27&with_watch_monetization_types=flatrate`)
+//   const data = await res.json()
+
+//   console.log(data);
+  
+// }
---
+++ server.js
@@ -0,0 +1,76 @@
+const http = require('http')
+const fs = require('fs')
+const url = require('url');
+const querystring = require('querystring');
+const figlet = require('figlet')
+
+const server = http.createServer((req, res) => {
+  const page = url.parse(req.url).pathname;
+  const params = querystring.parse(url.parse(req.url).query);
+  console.log(page);
+  if (page == '/') {
+    fs.readFile('index.html', function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      res.end();
+    });
+  }
+//   else if (page == '/api') {
+//     let pageNumber = params['pagenumber']
+//     console.log(pageNumber)
+//     async function serveNewMovie(){
+//     const res = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=585021f63795aea89ca90be073375167&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${pageNumber}&with_genres=27&with_watch_monetization_types=flatrate`)
+//     const data = await res.json()
+
+//     console.log(data);
+  
+// }
+
+//   console.log(data);
+//     res.writeHead(200, {'Content-Type': 'application/json'});
+    // if('student' in params){
+    //   if(params['student']== 'leon'){
+    //     res.writeHead(200, {'Content-Type': 'application/json'});
+    //     const objToJson = {
+    //       name: "leon",
+    //       status: "Boss Man",
+    //       currentOccupation: "Baller"
+    //     }
+    //     res.end(JSON.stringify(objToJson));
+    //   }//student = leon
+    //   else if(params['student'] != 'leon'){
+    //     res.writeHead(200, {'Content-Type': 'application/json'});
+    //     const objToJson = {
+    //       name: "unknown",
+    //       status: "unknown",
+    //       currentOccupation: "unknown"
+    //     }
+    //     res.end(JSON.stringify(objToJson));
+      }//student != leon
+    }//student if
+  }//else if
+  else if (page == '/css/style.css'){
+    fs.readFile('css/style.css', function(err, data) {
+      res.write(data);
+      res.end();
+    });
+  }else if (page == '/js/main.js'){
+    fs.readFile('js/main.js', function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/javascript'});
+      res.write(data);
+      res.end();
+    });
+  }else{
+    figlet('404!!', function(err, data) {
+      if (err) {
+          console.log('Something went wrong...');
+          console.dir(err);
+          return;
+      }
+      res.write(data);
+      res.end();
+    });
+  }
+});
+
+server.listen(8000);
